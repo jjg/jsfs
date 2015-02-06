0b7f4f06a98ec0c3eb9ddff4741d8683469a72f0
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-02-06 09:31:15 -0600

    updated config to handle multiple storage locations with constrained capacities

--- config.ex
+++ config.ex
@@ -1,6 +1,9 @@
 module.exports = {
-	STORAGE_PATH: './blocks/',
+	
+	STORAGE_LOCATIONS:[
+		{"path":"./blocks/","capacity":1024}
+		],
 	BLOCK_SIZE: 1048576,
 	LOG_LEVEL: 0,
 	SERVER_PORT: 7302
-}
+};
