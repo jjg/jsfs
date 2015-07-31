fa1059e4f97fc24f231dda14e1e01ba15d041910
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-07-31 13:51:59 -0500

    tested async superblock loading with 20MB+ superblock

--- unique_block_initializer.js
+++ unique_block_initializer.js
@@ -1,12 +1,22 @@
 // parse superblock and pass unique blocks back to parent
+var fs = require("fs");
+
 var unique_blocks = [];
 var superblock = null;
 
 process.on("message", function(message){
 	//console.log("message received");
-	if(message.hasOwnProperty("superblock")){
-		//console.log("superblock received");
-		superblock = message.superblock;
+	//console.log(JSON.stringify(message));
+	if(message.hasOwnProperty("load_superblock")){
+		//console.log("loading superblock from disk");
+		superblock = JSON.parse(fs.readFileSync("./blocks/superblock.json"));
+		//console.log("superblock loaded from disk");
+		process.send({superblock_loaded:true});
+		//console.log("loaded message sent to parent");
+	}
+	if(message.hasOwnProperty("process_superblock")){
+		//console.log("process_superblock message received");
+		//superblock = message.superblock;
 		for(var inode in superblock){
 			if(superblock.hasOwnProperty(inode)){
 				//console.log("inode selected");
@@ -23,5 +33,6 @@ process.on("message", function(message){
 			}
 		}
 		// todo: tell parent we're done & trigger deallocation of child
+		console.log("done processing superblock");
 	}
 });
