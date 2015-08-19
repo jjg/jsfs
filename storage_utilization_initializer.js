a1f813cfaef504a40a51778e962c7a3bce323247
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-08-19 10:09:00 -0500

    removed depreciated code

--- storage_utilization_initializer.js
+++ storage_utilization_initializer.js
@@ -4,23 +4,27 @@ var log = require("./jlog.js");
 var config = require("./config.js");
 
 log.level = config.LOG_LEVEL;
-log.path = "./jlog.log";
 
 process.on("message", function(message){
+	//log.message(log.DEBUG, "\tSUI: message");
 	if(message.hasOwnProperty("superblock") && message.hasOwnProperty("storage_locations")){
 		var superblock = message.superblock;
 		var storage_locations = message.storage_locations;
 		for(var inode in superblock){
+			//log.message(log.DEBUG, "\tSUI: inode");
 			if(superblock.hasOwnProperty(inode)){
 				var selected_inode = superblock[inode];
 				for(block in selected_inode.blocks){
+					//log.message(log.DEBUG, "\tSUI: block");
 					var selected_block = selected_inode.blocks[block];
 					for(var storage_location in storage_locations){
+						//log.message(log.DEBUG, "\tSUI: storage_location");
 						var selected_storage_location = storage_locations[storage_location];
 						if(fs.existsSync(selected_storage_location.path + selected_block.block_hash)){
+							//log.message(log.DEBUG, "\tSUI: existsSync");
 							// todo: update block's last_seen property in the live object back at the parent?
-							var block_data = fs.readFileSync(selected_storage_location.path + selected_block.block_hash);
-							selected_storage_location.usage += block_data.length;
+							var block_stats = fs.statSync(selected_storage_location.path + selected_block.block_hash);
+							selected_storage_location.usage += block_stats.size;
 						}
 					}
 				}
