84ab40254c865e9545472e6b0f853f60707635a5
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-10-01 14:19:06 -0500

    better migration feedback

--- experimental/migrate_superblock.js
+++ experimental/migrate_superblock.js
@@ -5,12 +5,13 @@ var crypto = require("crypto");
 console.log("Loading superblock...");
 var superblock = JSON.parse(fs.readFileSync("superblock.json"));
 console.log("Superblock loaded, indexing inodes...");
-var inodes_remaining = 0;
+var inodes_total = 0;
 for(inode in superblock){
 	if(superblock.hasOwnProperty(inode)){
-		inodes_remaining++;
+		inodes_total++;
 	}
 }
+var inodes_remaining = inodes_total;
 console.log("Superblock indexed, " + inodes_remaining + " inodes to process");
 
 // load next inode
@@ -22,27 +23,33 @@ for(inode in superblock){
 		var shasum = crypto.createHash("sha1");
 		shasum.update(selected_inode.url);
 		var inode_filename = shasum.digest("hex") + ".json";
-		console.log("Inode filename: " + inode_filename);
+		log("Inode filename: " + inode_filename);
 
 		// check if inode file exists
 		try{
 			// if inode file exists, load existing file
 			var existing_inode = JSON.parse(fs.readFileSync(inode_filename));
-			console.log("Inode file already exists, comparing versions...");
+			log("Inode file already exists, comparing versions...");
 			// compare versions
 			if(inode.version > existing_inode.version){
 				// replace existing file if the current inode is newer
-				console.log("Replacing existing inode file with newer version");
+				log("Replacing existing inode file with newer version");
 				fs.writeFileSync(inode_filename, JSON.stringify(selected_inode));
 			}
 		}catch(ex){
 			// if inode doesn't exist, save the one we have
-			console.log("Writing inode file to disk");
+			log("Writing inode file to disk");
 			fs.writeFileSync(inode_filename, JSON.stringify(selected_inode));
 		}
 
 		inodes_remaining--;
-		console.log("Inode " + selected_inode.fingerprint + " processed, " + inodes_remaining + " inodes remaining");
+		log("Inode " + selected_inode.fingerprint + " processed, " + inodes_remaining + " inodes remaining");
 	}
+}
+
+console.log("Superblock migration complete!");
+
+function log(message){
+	console.log(inodes_total + "/" + inodes_remaining + " - " + message);
 }
 
