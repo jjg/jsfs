2ef98d0b63478f54a4a289ea0d007ed105fa99bc
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-11-12 16:10:44 -0600

    updated example config to match current feature set

--- config.ex
+++ config.ex
@@ -4,9 +4,6 @@ module.exports = {
 		//{"path":"./blocks1/"},
 		//{"path":"./blocks2/"}
 	],
-	PEERS:[
-		//{"host":"host.domain.com","port":7302}
-	],
 	BLOCK_SIZE: 1048576,
 	LOG_LEVEL: 0,
 	SERVER_PORT: 7302,
