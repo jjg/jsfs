5c2fade0292da942475b9cd8a1d9c44b1d9a978d
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-12-03 10:27:44 -0600

    WIP on jsfsck

--- tools/jsfsck.js
+++ tools/jsfsck.js
@@ -11,10 +11,16 @@
 // if no repair options are specified, only analysis is performed
 
 // globals
+var fs = require("fs");
 var config_path = null
 var mode = null;
+var unique_block_index = [];
+var pool_size_in_bytes = 0;
+var on_disk_size_in_bytes = 0;
+var files_stored = 0;
+var errors = [];
 
-// TODO: parse command line parameters
+// parse command line parameters
 config_path = process.argv[2];
 mode = process.argv[3];
 
@@ -23,8 +29,29 @@ if(!config_path || !mode){
   process.exit(0);
 }
 
-// TODO: load configuration
+// load configuration
+var config = null;
+try{
+  config = require(config_path);
+  if(config.STORAGE_LOCATIONS.length < 1){
+    console.log("No storage locations configured, exiting");
+    process.exit(0);
+  }
+} catch(exception) {
+  console.log("Error reading configuration: " + exception);
+}
 
+
+for(location in config.STORAGE_LOCATIONS){
+  var selected_location = config.STORAGE_LOCATIONS[location];
+  
+  var files = fs.readdirSync(selected_location.path);
+  
+  for(file in files){
+    var selected_file = files[file];
+    if(selected_file.indexOf(".json") > -1){
+      files_stored++;
+      var inode = JSON.parse(fs.readFileSync(selected_file));
 
 // TODO: iterate over blocks and create uniquie block index
 
