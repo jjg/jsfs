650e45ed4d32341c72841837ae6730dea9e95660
Jason J. Gullickson <jason.gullickson@gmail.com>
2016-02-26 15:16:25 -0600

    added config.js back to repo to try and get CI to work

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	STORAGE_LOCATIONS:[
+		{"path":"./blocks/"}
+		//{"path":"./blocks1/"},
+		//{"path":"./blocks2/"}
+	],
+	BLOCK_SIZE: 1048576,
+	LOG_LEVEL: 0,
+	SERVER_PORT: 7302,
+	REQUEST_TIMEOUT: 30 // minutes,
+};
