ed98a906f2132f1e73607c45011434140e72f07b
Marc Brakken <marc.brakken@gmail.com>
2016-10-06 00:26:02 -0500

    add process for sweeping up files that errored according to logs

---
+++ tools/source_ips.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'jsfs-g.murfie.com' : '10.240.123.180',
+  'jsfs2.murfie.com'  : '10.240.123.180',
+  'jsfs3.murfie.com'  : '10.240.0.3',
+  'jsfs4.murfie.com'  : '10.240.0.5',
+  'jsfs5.murfie.com'  : '10.240.0.6',
+  'jsfs6.murfie.com'  : '10.240.0.7',
+  'jsfs7.murfie.com'  : '10.240.0.8',
+  'jsfs8.murfie.com'  : '10.240.0.9',
+  'jsfs9.murfie.com'  : '10.240.0.10',
+  'jsfs10.murfie.com' : '10.240.0.11',
+  'jsfs11.murfie.com' : '10.240.0.12',
+  'jsfs12.murfie.com' : '10.240.0.13',
+  'jsfs13.murfie.com' : '10.240.0.14',
+  'jsfs14.murfie.com' : '10.240.0.15',
+  'jsfs15.murfie.com' : '10.240.0.17',
+  'jsfs16.murfie.com' : '10.240.0.19',
+  'jsfs17.murfie.com' : '10.240.0.20',
+  'jsfs18.murfie.com' : '10.240.0.21',
+  'jsfs19.murfie.com' : '10.240.0.23',
+  'jsfs21.murfie.com' : '10.240.0.24',
+  'jsfs22.murfie.com' : '10.240.0.25'
+};
