34b0e8b11ce3ee5d9ac2707fb415505d5d4e31ab
jjg <mr@jasongullickson.com>
2024-09-20 08:55:29 -0500

    Minimal config to test running on Railway.app

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  STORAGE_LOCATIONS:[
+    {"path":"./blocks/"}
+  ],
+  BLOCK_SIZE: 1048576,
+  LOG_LEVEL: 0,
+  SERVER_PORT: 7302,
+  REQUEST_TIMEOUT: 30, // minutes
+  CONFIGURED_STORAGE: "fs",
+  STATIC_ACCESS_KEYS: ["foobar69"]
+};
